cbf33785cb8254719e0aaf4f3b1e1da34f3dc736
Ibrahim Aouli <ibztheman2@gmail.com>
2024-12-23 11:02:31 +1100

    global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 // src/App.js
 import React from "react";
 import AppRouter from "./router/AppRouter";
-import "./styles/global.css";
-
 const App = () => {
   return (
     <div className="App">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Updated import for React 18
 import App from "./App";
+import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // Create root
