4978e1edbddc4c4e0563a42a2be0fdeac07a48c2
Ibrahim Aouli <ibztheman2@gmail.com>
2024-12-26 00:07:27 +1100

    updated some dependencies

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json()); // Parse JSON request bodies
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
